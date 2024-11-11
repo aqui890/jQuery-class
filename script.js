9e2c3ba8298dc7cbeb734972643797e448667144
aqui890 <akqwy980@gmail.com>
2024-11-11 11:49:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const popupopen = document.querySelector(".open-pop");
+// const popup = document.querySelector(".pop");
+// const closebtn = document.querySelector(".close-pop");
+
+// popupopen.addEventListener("click", function() {
+//     popup.style.display = "block";
+// })
+
+// closebtn.addEventListener("click", function() {
+//     popup.style.display = "none";
+// });
