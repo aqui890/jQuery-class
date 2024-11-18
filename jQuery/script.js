0a00b9e01756cef26f2595ffe7fad255f97ccfa1
aqui890 <akqwy980@gmail.com>
2024-11-18 11:52:50 +0900

    Add files via upload

---
+++ jQuery/script.js
@@ -0,0 +1,11 @@
+$("h1").hide();
+$("h1").slideDown("slow");
+$(".moregreetings").hide("slow");
+
+$("#box, h1").css("color", "blue");
+
+$("h1#box").css("color", "blue");
+
+$(".news").parent().css("backgroundColor", "red");
+$("ul > li:nth-child(1)").css("color", "blue");
+$("ul > li:nth-child(2)").css("color", "blue");
